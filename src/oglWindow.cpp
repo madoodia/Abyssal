d75d8d6ac677149e40c2cb5b41e9c99b8c25eeea
Mahmoodreza Aarabi <madoodia@gmail.com>
2019-09-28 05:54:07 -0700

    2  trianlges are created by glDrawArrays

--- src/oglWindow.cpp
+++ src/oglWindow.cpp
@@ -101,36 +101,28 @@ void OGLWindow::initializeGL()
 	// Vertex Data
 	float vertices[] =
 	{
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
-	};
+		// First Triangle
+		-0.5f,  0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 
-	unsigned int indices[] =   // note that we start from 0!
-	{
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
+		// Second Triangle
+		0.5f,  0.5f, 0.0f,
+		-0.5f,  -0.5f, 0.0f,
+		0.5f,  -0.5f, 0.0f
 	};
 
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
 
 	glBindVertexArray(vao);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; 
-	// keep the EBO bound.
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
@@ -143,13 +135,12 @@ void OGLWindow::paintGL()
 
 	glUseProgram(shaderProgram);
 	glBindVertexArray(vao);
-	// glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
 	// glDrawElements(GL_LINES, 6, GL_UNSIGNED_INT, 0);
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void OGLWindow::keyPressEvent(QKeyEvent* event)
